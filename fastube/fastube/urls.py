a5f76f3c23fe69b828f45eec308cc4eaaa72235e
wonju <gahuins@gmail.com>
2016-06-22 19:27:38 +0900

    init LogoutView: perform logout and redirect to login page

--- fastube/fastube/urls.py
+++ fastube/fastube/urls.py
@@ -24,4 +24,5 @@ urlpatterns = [
 
     url(r'^signup/$', SignupView.as_view(), name="signup"),
     url(r'^login/$', LoginView.as_view(), name="login"),
+    url(r'^logout/$', LogoutView.as_view(), name="logout"),
 ]
